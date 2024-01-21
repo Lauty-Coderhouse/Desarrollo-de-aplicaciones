af1fa33d93939855c0ce1ea3fd1b10124e568f4f
LautyDev <86755961+LautyDev@users.noreply.github.com>
2024-01-21 11:21:20 -0300

    Desafío terminado ⭐

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
+  const handlePress = () =>
+    Linking.openURL(
+      "https://github.com/Lauty-Coderhouse/Desarrollo-de-aplicaciones/tree/desafio-1"
+    );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 40 }}>Hola, Coder! 👋</Text>
+      <Text style={{ fontSize: 25, paddingTop: 20 }}>Este es otro texto</Text>
+
+      <Image
+        source={{
+          uri: "https://styles.redditmedia.com/t5_5blkvq/styles/communityIcon_t5ji6wuld2z71.jpg?format=pjpg&s=1672be49333cd14749784be8bbc3c968a033d8e0",
+        }}
+        style={styles.image}
+      />
+
+      <TouchableOpacity onPress={handlePress} style={styles.button}>
+        <Icon name="github" size={30} color="white" />
+        <Text style={{ color: "white", fontSize: 20, marginLeft: 10 }}>
+          {" "}
+          Ver repositorio
+        </Text>
+      </TouchableOpacity>
+
+      <View style={styles.footer}>
+        <Text style={{ color: "white", fontSize: 20 }}>Lautaro Chavero</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +47,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    margin: 20,
+    width: 200,
+    height: 200,
+    borderRadius: 20,
+  },
+  button: {
+    margin: 50,
+    backgroundColor: "black",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "black",
+    padding: 10,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
